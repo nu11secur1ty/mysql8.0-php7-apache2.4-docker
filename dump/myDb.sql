069a3c2224ff1d60034142a888491ec01f890d7b
nu11secur1ty <venvaropt@gmail.com>
2019-07-11 22:04:54 +0300

    Update myDb.sql

--- dump/myDb.sql
+++ dump/myDb.sql
@@ -15,9 +15,9 @@ CREATE TABLE `Person` (
 
 
 INSERT INTO `Person` (`id`, `name`) VALUES
-(1, 'William'),
-(2, 'Marc'),
-(3, 'John');
+(1, 'Igriva'),
+(2, 'Ebliva'),
+(3, 'Rusa');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
